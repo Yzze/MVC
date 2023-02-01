f57c295cc19c5d54996af68761edb2bc466f2e6b
Yzee <yan.zhigurs@mail.ru>
2023-02-01 19:45:04 +0400

    add methods in Controller

--- MVC/Source/Scenes/Controller/SettingsViewController.swift
+++ MVC/Source/Scenes/Controller/SettingsViewController.swift
@@ -6,3 +6,34 @@
 //
 
 import UIKit
+
+final class ViewController: UIViewController {
+    
+    var table = Table()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    private func setupView() {
+       title = "Настройки"
+       navigationController?.navigationBar.prefersLargeTitles = true
+       view.backgroundColor = .systemGray5
+   }
+   
+    private func setupHierarchy() {
+        view.addSubview(table.tableView)
+   }
+   
+    private func setupLayout() {
+       NSLayoutConstraint.activate([
+        table.tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        table.tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        table.tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        table.tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+       ])
+   }
+}
--- MVC/Source/Scenes/Model/Model.swift
+++ MVC/Source/Scenes/Model/Model.swift
@@ -12,66 +12,136 @@ class Model {
     
     func configure() -> [Section]{
         models.append(Section(options: [
-            .topCell(model: TopCellOption(icon: UIImage(named: "swift"), title: "Ян Жигурс", subtitleText: "Apple ID, iCloud, контент и покупки", handler: {
-                print("Главная ячейка")
-            }))
+            .topCell(model: TopCellOption(icon: UIImage(named: "swift"),
+                                          title: "Ян Жигурс",
+                                          subtitleText: "Apple ID, iCloud, контент и покупки",
+                                          handler: {
+                                              print("Главная ячейка")
+                                          }))
         ]))
         
         models.append(Section(options: [
-            .switchCell(model: SettingsSwitchOption(title: "Авиарежим", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, isOn: false, handler: {
-                print("Нажатие на Авиарежим")
-            })),
-            .staticCell(model: SettingsOption(title: "Wi-Fi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemBlue, optionalText: "Не подключено", handler: {
-                print("Нажатие на WiFi")
-            })),
-            .staticCell(model: SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "dot.radiowaves.left.and.right"), iconBackgroundColor: .systemBlue, optionalText: "Выкл.", handler: {
-                print("Нажатие на Bluetooth")
-            })),
-            .staticCell(model: SettingsOption(title: "Cотовая связь", icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBackgroundColor: .systemGreen, optionalText: "", handler: {
-                print("Нажатие на Cотовая связь")
-            })),
-            .staticCell(model: SettingsOption(title: "Режим модема", icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemGreen, optionalText: "Выкл.", handler: {
-                print("Нажатие на Режим модема")
-            })),
-            .switchCell(model: SettingsSwitchOption(title: "VPN", icon: UIImage(systemName: "lock.shield"), iconBackgroundColor: .systemBlue, isOn: false, handler: {
-                print("Нажатие на VPN")
-            }))])
+            .switchCell(model: SettingsSwitchOption(title: "Авиарежим",
+                                                    icon: UIImage(systemName: "airplane"),
+                                                    iconBackgroundColor: .systemOrange,
+                                                    isOn: false,
+                                                    handler: {
+                                                        print("Нажатие на Авиарежим")
+                                                    })),
+            .staticCell(model: SettingsOption(title: "Wi-Fi",
+                                              icon: UIImage(systemName: "wifi"),
+                                              iconBackgroundColor: .systemBlue,
+                                              optionalText: "Не подключено",
+                                              handler: {
+                                                  print("Нажатие на WiFi")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Bluetooth",
+                                              icon: UIImage(systemName: "dot.radiowaves.left.and.right"),
+                                              iconBackgroundColor: .systemBlue,
+                                              optionalText: "Выкл.",
+                                              handler: {
+                                                  print("Нажатие на Bluetooth")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Cотовая связь",
+                                              icon: UIImage(systemName: "antenna.radiowaves.left.and.right"),
+                                              iconBackgroundColor: .systemGreen,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Cотовая связь")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Режим модема",
+                                              icon: UIImage(systemName: "personalhotspot"),
+                                              iconBackgroundColor: .systemGreen,
+                                              optionalText: "Выкл.",
+                                              handler: {
+                                                  print("Нажатие на Режим модема")
+                                              })),
+            .switchCell(model: SettingsSwitchOption(title: "VPN",
+                                                    icon: UIImage(systemName: "lock.shield"),
+                                                    iconBackgroundColor: .systemBlue,
+                                                    isOn: false,
+                                                    handler: {
+                                                        print("Нажатие на VPN")
+                                                    }))
+        ])
         )
         
         models.append(Section(options: [
-            .staticCell(model: SettingsOption(title: "Уведомления", icon: UIImage(systemName: "bell.badge"), iconBackgroundColor: .systemRed, optionalText: "", handler: {
-                print("Нажатие на Уведомления")
-            })),
-            .staticCell(model: SettingsOption(title: "Звуки", icon: UIImage(systemName: "speaker.wave.3"), iconBackgroundColor: .systemPink, optionalText: "", handler: {
-                print("Нажатие на Звуки")
-            })),
-            .staticCell(model: SettingsOption(title: "Фокусирование", icon: UIImage(systemName: "moon"), iconBackgroundColor: .systemPurple, optionalText: "", handler: {
-                print("Нажатие на Фокусирование")
-            })),
-            .staticCell(model: SettingsOption(title: "Экранное время", icon: UIImage(systemName: "hourglass"), iconBackgroundColor: .systemPurple, optionalText: "", handler: {
-                print("Нажатие на Экранное время")
-            }))])
+            .staticCell(model: SettingsOption(title: "Уведомления",
+                                              icon: UIImage(systemName: "bell.badge"),
+                                              iconBackgroundColor: .systemRed,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Уведомления")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Звуки",
+                                              icon: UIImage(systemName: "speaker.wave.3"),
+                                              iconBackgroundColor: .systemPink,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Звуки")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Фокусирование",
+                                              icon: UIImage(systemName: "moon"),
+                                              iconBackgroundColor: .systemPurple,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Фокусирование")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Экранное время",
+                                              icon: UIImage(systemName: "hourglass"),
+                                              iconBackgroundColor: .systemPurple,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Экранное время")
+                                              }))
+        ])
         )
         
         models.append(Section(options: [
-            .badgeCell(model: SettingsBadgeOption(title: "Основные", icon: UIImage(systemName: "gear"), iconBackgroundColor: .systemGray, badgeLabel: "1", handler: {
-                print("Нажатие на Основные")
-            })),
-            .staticCell(model: SettingsOption(title: "Пункт управления", icon: UIImage(systemName: "switch.2"), iconBackgroundColor: .systemGray, optionalText: "", handler: {
-                print("Нажатие на Пункт управления")
-            })),
-            .staticCell(model: SettingsOption(title: "Экран и яркость", icon: UIImage(systemName: "textformat.size"), iconBackgroundColor: .systemBlue, optionalText: "", handler: {
-                print("Нажатие на Экран и яркость")
-            })),
-            .staticCell(model: SettingsOption(title: "Экран домой", icon: UIImage(systemName: "apps.iphone"), iconBackgroundColor: .systemBlue, optionalText: "", handler: {
-                print("Нажатие на Экран домой")
-            })),
-            .staticCell(model: SettingsOption(title: "Универсальный доступ", icon: UIImage(systemName: "figure.wave.circle"), iconBackgroundColor: .systemBlue, optionalText: "", handler: {
-                print("Нажатие на Универсальный доступ")
-            })),
-            .staticCell(model: SettingsOption(title: "Обои", icon: UIImage(systemName: "paintbrush.fill"), iconBackgroundColor: .systemCyan, optionalText: "", handler: {
-                print("Нажатие на Обои")
-            }))])
+            .badgeCell(model: SettingsBadgeOption(title: "Основные",
+                                                  icon: UIImage(systemName: "gear"),
+                                                  iconBackgroundColor: .systemGray,
+                                                  badgeLabel: "1",
+                                                  handler: {
+                                                      print("Нажатие на Основные")
+                                                  })),
+            .staticCell(model: SettingsOption(title: "Пункт управления",
+                                              icon: UIImage(systemName: "switch.2"),
+                                              iconBackgroundColor: .systemGray,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Пункт управления")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Экран и яркость",
+                                              icon: UIImage(systemName: "textformat.size"),
+                                              iconBackgroundColor: .systemBlue,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Экран и яркость")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Экран домой",
+                                              icon: UIImage(systemName: "apps.iphone"),
+                                              iconBackgroundColor: .systemBlue,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Экран домой")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Универсальный доступ",
+                                              icon: UIImage(systemName: "figure.wave.circle"),
+                                              iconBackgroundColor: .systemBlue,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Универсальный доступ")
+                                              })),
+            .staticCell(model: SettingsOption(title: "Обои",
+                                              icon: UIImage(systemName: "paintbrush.fill"),
+                                              iconBackgroundColor: .systemCyan,
+                                              optionalText: "",
+                                              handler: {
+                                                  print("Нажатие на Обои")
+                                              }))
+        ])
         )
         return models
     }
