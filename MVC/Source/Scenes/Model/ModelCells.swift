7a833251d2b61787a2bfd3304a6f673c77c4c280
Yzee <yan.zhigurs@mail.ru>
2023-02-01 19:28:36 +0400

    add Model

--- MVC/Source/Scenes/Model/ModelCells.swift
+++ MVC/Source/Scenes/Model/ModelCells.swift
@@ -5,4 +5,46 @@
 //  Created by Ян Жигурс on 01.02.2023.
 //
 
-import Foundation
+import UIKit
+
+struct Section {
+    let options: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingsSwitchOption)
+    case topCell(model: TopCellOption)
+    case badgeCell(model: SettingsBadgeOption)
+}
+
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let optionalText: String
+    let handler: (() -> Void)
+}
+
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let isOn: Bool
+    let handler: (() -> Void)
+}
+
+struct TopCellOption {
+    let icon: UIImage?
+    let title: String
+    let subtitleText: String
+    let handler: (() -> Void)
+}
+
+struct SettingsBadgeOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let badgeLabel: String
+    let handler: (() -> Void)
+}
