fbb3a5cefe1c6ef16ddc9faea49bd3fd96905186
Yzee <yan.zhigurs@mail.ru>
2023-02-01 19:50:55 +0400

    add view in scene delegate and image

--- MVC/Source/App/SceneDelegate.swift
+++ MVC/Source/App/SceneDelegate.swift
@@ -10,6 +10,11 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
